71d97dac801242810f8b7c8a5a477ece560628b7
Nilton Santos <niltonsantosmf2@gmail.com>
2021-06-24 13:09:31 -0300

    atualização tela de jogadores

---
+++ src/screens/AppointmentDetails/index.tsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import { Fontisto } from '@expo/vector-icons'
+import { BorderlessButton } from 'react-native-gesture-handler'
+
+import { View, Text, ImageBackground, FlatList } from 'react-native'
+
+import { ListDivider } from '../../components/ListDivider'
+import { Background } from '../../components/Background'
+import { ListHeader } from '../../components/ListHeader'
+import { ButtonIcon } from '../../components/ButtonIcon'
+
+import { Header } from '../../components/Header'
+import { Member } from '../../components/Member'
+
+
+import { theme } from '../../global/styles/theme'
+import { styles } from './styles'
+import BannerImg from '../../assets/banner.png'
+
+export function AppointmentDetails() {
+    const members = [
+        {
+            id: '1',
+            username: 'Nilton',
+            avatar_url: 'https://github.com/Nilton-santos25.png',
+            status: 'online'
+        },
+        {
+            id: '2',
+            username: 'Nilton',
+            avatar_url: 'https://github.com/Nilton-santos25.png',
+            status: 'offline'
+        }
+    ]
+    return(
+        <Background>
+            <Header 
+                title="Detalhes"
+                action={
+                    <BorderlessButton>
+                        <Fontisto 
+                            name="share"
+                            size={24}
+                            color={theme.colors.primary}
+                        />
+                    </BorderlessButton>
+                }
+            />
+
+            <ImageBackground 
+                source={BannerImg}
+                style={styles.banner}
+            >
+                <View style={styles.bannerContent}>
+                    <Text style={styles.title}>
+                        Lendários
+                    </Text>
+
+                    <Text style={styles.subtitle}>
+                        É hoje que vamos chegar ao challender sem perder uma partida da md10
+                    </Text>
+                </View>
+            </ImageBackground>
+
+            <ListHeader
+                title = "jogadores"
+                subtitle = "Total 3"
+            />
+
+            <FlatList
+                data={members}
+                keyExtractor={item => item.id}
+                renderItem={({ item}) => (
+                    <Member data={item} />
+                )}
+                ItemSeparatorComponent={() => <ListDivider />}
+                style={styles.members}
+            />
+
+            <View style={styles.footer}>
+                <ButtonIcon title="Entrar na Partida" />
+            </View>
+        </Background>
+    )
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import { View, FlatList } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import { CategorySelect } from '../../components/CategorySelect';
 import { Appointment } from '../../components/Appointment';
 import { ListDivider } from '../../components/ListDivider';
 import { ListHeader } from '../../components/ListHeader';
+import { Background } from '../../components/Background';
 import { ButtonAdd } from '../../components/ButtonAdd';
 import { Profile } from '../../components/Profile';
 
@@ -12,6 +14,8 @@ import { styles } from './styles';
 
 export function Home() {
   const [category, setCategory] = useState('');
+
+  const navigation = useNavigation()
 
   const appointments = [
     {
@@ -43,37 +47,50 @@ export function Home() {
 
   function handleCategorySelect(categoryId: string) {
     categoryId === category ? setCategory('') : setCategory(categoryId);
-  }  
+  } 
+  
+  function handleAppointmentDetails(){
+    navigation.navigate('AppointmentDetails')
+  }
+
+  function handleAppointmentCreate(){
+    navigation.navigate('AppointmentCreate')
+  }
 
   return (
-    <View>
-      <View style={styles.header}>
-        <Profile />
-        <ButtonAdd />
-      </View>
-    
-      <CategorySelect 
-        categorySelected={category}
-        setCategory={handleCategorySelect}
-      />
-
-      <View style={styles.content}>
-        <ListHeader 
-          title="Partidas agendadas"
-          subtitle="Total 6"
+    <Background>
+      <View>
+        <View style={styles.header}>
+          <Profile />
+          <ButtonAdd onPress={handleAppointmentCreate}/>
+        </View>
+      
+        <CategorySelect 
+          categorySelected={category}
+          setCategory={handleCategorySelect}
         />
 
-        <FlatList 
-            data={appointments}
-            keyExtractor={item => item.id}
-            renderItem={({ item }) => (
-            <Appointment data={item} />            
-          )}
-          ItemSeparatorComponent={() => <ListDivider />}
-          style={styles.matches}
-          showsVerticalScrollIndicator={false}
-        />
+        <View style={styles.content}>
+          <ListHeader 
+            title="Partidas agendadas"
+            subtitle="Total 6"
+          />
+
+          <FlatList 
+              data={appointments}
+              keyExtractor={item => item.id}
+              renderItem={({ item }) => (
+              <Appointment 
+                data={item}
+                onPress={handleAppointmentDetails}
+              />            
+            )}
+            ItemSeparatorComponent={() => <ListDivider />}
+            style={styles.matches}
+            showsVerticalScrollIndicator={false}
+          />
+        </View>
       </View>
-    </View>
+    </Background>
   );  
 }
